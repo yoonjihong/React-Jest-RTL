c66b4fe4ad1c02b52892ad02a65ee0296135f2d3
YJH <47561303+yoonjihong@users.noreply.github.com>
2022-09-15 10:12:19 +0900

    테트

--- src/components/Todo/todoList.test.tsx
+++ src/components/Todo/todoList.test.tsx
@@ -19,11 +19,13 @@ afterAll(() => {
 });
 
 describe('투두리스트', () => {
-  render(<Home />);
+  const mount = render(<Home />);
 
   it('투두 등록 테스트', async () => {
     // 컴포넌트가 제대로 렌더링 됐는지 확인하기 위해 내용 확인
     screen.getByText('TODO 리스트'); // 성공
+
+    mount.getByText('TODO 리스트');
 
     // 아이템 추가 버튼 클릭
     const addButton = screen.getByTestId('addButton');
