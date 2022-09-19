1729e21ee741bc6b5e1db375c6c55cb42e692c0d
YJH <47561303+yoonjihong@users.noreply.github.com>
2022-09-19 11:21:04 +0900

    테스트

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Todo from '~components/Todo';
 import AddForm from '~components/Todo/AddForm';
+import asdf from '~components/asdf';
 
 const Home = () => {
   const [list, setList] = useState(['첫번째 아이템', '두번째 아이템']);
@@ -19,8 +20,16 @@ const Home = () => {
           setList(prev => [...prev, data]);
         }}
       />
+
+      <Test />
     </div>
   );
 };
 
 export default Home;
+
+const Test = () => {
+  useEffect(() => {});
+
+  return <div>asdfsd</div>;
+};
